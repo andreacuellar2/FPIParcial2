4e368010fe1e6535e4e17028b819d5eef51c1c83
Diannie <mb15004@ues.edu.sv>
2017-10-31 01:57:08 -0600

    tratando de implementar el get articulo

--- JS/leer.js
+++ JS/leer.js
@@ -1,3 +1,43 @@
+function pubRequestJSON(){
+  var pubWanted = window.localStorage.getItem("idPubWanted");
+  var pubRequest = new XMLHttpRequest();
+  pubRequest.open('GET', 'DATA/publicaciones.json');
+  pubRequest.onload = function() {
+    if (pubRequest.status >= 200 && pubRequest.status < 400) {
+      var pubData = JSON.parse(pubRequest.responseText);
+      pubSetHTML(pubData, pubWanted);
+    } else {
+      console.log("Se conecto con el servidor pero ocurrio un error");
+    }
+  };
+    pubRequest.onerror = function() {
+      console.log("Error al conectar con el servidor");
+    };
+    pubRequest.send();
+  }
+
+  function pubSetHTML(pubData, pubWanted) {
+    for (i in pubData) {
+         if (pubData[i].id == pubWanted) {
+           var publicacion = new Publicacion(pubData[i].id, pubData[i].idUsuario, pubData[i].titulo, pubData[i].contenido, pubData[i].idCategoria, pubData[i].imgSrc, pubData[i].comentarios, pubData[i].fecha);
+         }
+       }
+      document.getElementById("nombreArticulo").innerHTML = publicacion.titulo;
+      document.getElementById("imgArticulo").src = "IMG/publicaciones/"+publicacion.imgSrc;
+      document.getElementById("fechaArticulo").innerHTML = publicacion.fecha;
+      document.getElementById("contenidoArticulo").src = publicacion.contenido;
+      document.getElementById("numeroComentarios").innerHTML = "Comentarios:"+publicacion.comentarios.lenght;
+      var listaComentarios = document.getElementById("comentarios");
+      for (var i in publicacion.comentarios) {
+        var nodo = document.createElement("LI");
+        var textnode = document.createTextNode(publicacion.comentarios[i]);
+          node.appendChild(textnode);
+          document.getElementById("myList").appendChild(node);
+      }
+  }
+
+
+
 function TopNavSesion(){
   var prueba =  window.localStorage.getItem("idUserLogged");
   if (prueba == "null"){
@@ -7,6 +47,7 @@ function TopNavSesion(){
     document.getElementById('navPerfil').style.display = 'none';
     document.getElementById('navPublicar').style.display= 'none';
     document.getElementById('navLogout').style.display= 'none';
+    document.getElementById('comentando').style.display= 'none';
   }else {
     document.getElementById('areaPerfil').style.display = 'block';
     document.getElementById('navLogin').style.display = 'none';
---
+++ JS/publicacion.js
@@ -0,0 +1,10 @@
+function Publicacion(id, idUsuario, titulo, contenido, idCategoria, imgSrc, comentarios, fecha) {
+  this.id = id;
+  this.idUsuario = idUsuario;
+  this.titulo = titulo;
+  this.contenido = contenido;
+  this.idCategoria = idCategoria;
+  this.imgSrc = imgSrc;
+  this.comentarios = comentarios;
+  this.fecha = fecha;
+}
