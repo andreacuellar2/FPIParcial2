9d295a39df6ddc1f2f675f09c4fe22f501ef840a
Juan Pleitez <juanpc13lolol@gmail.com>
2017-10-31 13:27:46 -0600

    Nuevos metodos para usar en categoria

--- JS/categorias.js
+++ JS/categorias.js
@@ -135,3 +135,64 @@ function LogOut() {
   localStorage.setItem("idUserLogged", null);
   window.location.replace("index.html");
 }
+function jsonReadFileVariablesUser() {
+  var userRequest = new XMLHttpRequest();
+  userRequest.open('GET', 'DATA/usuarios.json');
+  userRequest.onload = function() {
+    if (userRequest.status >= 200 && userRequest.status < 400) {
+      var userData = JSON.parse(userRequest.responseText);
+      jsonReadFilePublicaciones(userData);
+    } else {
+      console.log("Se conectó con el servidor pero ocurrió un error");
+    }
+  };
+    userRequest.onerror = function() {
+      console.log("Error al conectar con el servidor");
+    };
+    userRequest.send();
+}
+function jsonReadFilePublicaciones(allUsers) {
+  var userRequest = new XMLHttpRequest();
+  userRequest.open('GET', 'DATA/publicaciones.json');
+  userRequest.onload = function() {
+    if (userRequest.status >= 200 && userRequest.status < 400) {
+      var userPost = JSON.parse(userRequest.responseText);
+      jsonReadFileCategorias(allUsers,userPost);
+    } else {
+      console.log("Se conectó con el servidor pero ocurrió un error");
+    }
+  };
+    userRequest.onerror = function() {
+      console.log("Error al conectar con el servidor");
+    };
+    userRequest.send();
+}
+function jsonReadFileCategorias(allUsersData, allPublicaciones) {//Codigo para usar en generar entradas
+  var userRequest = new XMLHttpRequest();
+  userRequest.open('GET', 'DATA/categorias.json');
+  userRequest.onload = function() {
+    if (userRequest.status >= 200 && userRequest.status < 400) {
+      var allCategorias = JSON.parse(userRequest.responseText);
+      ConsumiendoTodo(allUsersData,allPublicaciones,allCategorias);
+    } else {
+      console.log("Se conectó con el servidor pero ocurrió un error");
+    }
+  };
+    userRequest.onerror = function() {
+      console.log("Error al conectar con el servidor");
+    };
+    userRequest.send();
+}
+function ConsumiendoTodo(todosUsuarios, todasPublicaciones, todasCategorias) {//Hacer esto nuestro main
+  var idUsuarioLogeado = parseInt(window.localStorage.getItem("idUserLogged"));
+
+
+
+
+
+
+
+
+
+
+}
