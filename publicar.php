5029efe3ad72f5a91197a79445866b4c8d28257d
Juan Pleitez <juanpc13lolol@gmail.com>
2017-11-01 13:33:20 -0600

    notificar que se ha publicado un post

--- publicar.php
+++ publicar.php
@@ -18,7 +18,8 @@ if ($_POST) {
     $todasPubliArray[] = $regPublicacion;
     $finalTodadPubli = json_encode($todasPubliArray);
     file_put_contents('DATA/publicaciones.json', $finalTodadPubli);
-    header( "Location: perfil.html" );
+    echo '<script type="text/javascript">alert("Se ha registrado con Exito");</script>';
+    header( "Location: index.html" );
     die();
   }else {
     header("Location: publicar.php?NoExisteArchivo");
