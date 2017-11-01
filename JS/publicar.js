fd096cc02e0f76f63e23e5bdaa10e6feaded1577
Diannie <mb15004@ues.edu.sv>
2017-11-01 14:43:30 -0600

    completando publicar

--- JS/publicar.js
+++ JS/publicar.js
@@ -26,6 +26,8 @@ function CargarCategorias(){
     userRequest.send();
 }
 function ValidarSesion() {
+  document.getElementById('masComentado1').style.display = 'none';
+  document.getElementById('masComentado2').style.display = 'none';
   var sessionLog = window.localStorage.getItem("idUserLogged");
   if(sessionLog == "null" || sessionLog == null){
     window.location.replace("login.html");
@@ -45,3 +47,56 @@ function irAPerfil(){
 function irAPublicar(){
   window.location.replace("publicar.php");
 }
+////////////////////////////Redirecciones mas comentados///////////////////////
+function redireccionTres() {
+  id = parseInt(document.getElementById('masComentado1').innerHTML);
+  localStorage.setItem("idPubWanted", null);
+  localStorage.setItem("idPubWanted", id);
+  window.location.replace("leer.php");
+}
+function redireccionCuatro() {
+  id = parseInt(document.getElementById('masComentado2').innerHTML);
+  localStorage.setItem("idPubWanted", null);
+  localStorage.setItem("idPubWanted", id);
+  window.location.replace("leer.php");
+}
+function pubRequestJSON(){
+  var pubRequest = new XMLHttpRequest();
+  pubRequest.open('GET', 'DATA/publicaciones.json');
+  pubRequest.onload = function() {
+    if (pubRequest.status >= 200 && pubRequest.status < 400) {
+      var pubData = JSON.parse(pubRequest.responseText);
+      PubSetHTML(pubData);
+    } else {
+      console.log("Se conecto con el servidor pero ocurrio un error");
+    }
+  };
+    pubRequest.onerror = function() {
+      console.log("Error al conectar con el servidor");
+    };
+    pubRequest.send();
+  }
+function PubSetHTML(pubData) {
+  ///////////////////Mas comentados//////////////////////////
+  var numComentarios = [];
+  for (var i in pubData) {
+    numComentarios.push({"coments":pubData[i].comentarios.length, "idPubComents":pubData[i].id});
+  }
+  numComentarios.sort(function(a, b){return b.coments-a.coments});
+    for (var i in pubData) {
+      ///////////////////////Mas comentada 1/////////////////////////////////////
+      if (numComentarios[0].idPubComents == pubData[i].id) {
+        document.getElementById("masComentado1").innerHTML = pubData[i].id;
+        document.getElementById("imgMasComentado1").src = "IMG/publicaciones/"+ pubData[i].imgSrc;
+        document.getElementById("tituloMasComentado1").innerHTML = pubData[i].titulo;
+        document.getElementById("resumenMasComentado1").innerHTML = pubData[i].contenido;
+      }
+      ///////////////////////Mas comentada 2/////////////////////////////////////
+      if (numComentarios[1].idPubComents == pubData[i].id) {
+        document.getElementById("masComentado2").innerHTML = pubData[i].id;
+        document.getElementById("imgMasComentado2").src = "IMG/publicaciones/"+ pubData[i].imgSrc;
+        document.getElementById("tituloMasComentado2").innerHTML = pubData[i].titulo;
+        document.getElementById("resumenMasComentado2").innerHTML = pubData[i].contenido;
+      }
+    }
+  }
